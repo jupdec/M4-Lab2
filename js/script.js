4e69e6edbdaa3317cbc1d1a50072549408876d97
Meeta Kapoor <47625349+meeta09@users.noreply.github.com>
2023-02-20 00:09:03 -0800

    Minor Updates
    Formatting changes

--- js/script.js
+++ js/script.js
@@ -6,32 +6,30 @@ let employeeArray = [
     ['EMP-003', 'Shayla Smith', '12448', 'smiths234@sdsu.edu', 'Engineering'],
     ['EMP-004', 'Anothny Roman', '13571', 'aanthony23@sdsu.edu', 'Marketing'],
     ['EMP-005', 'Jesus Hernandez', '98176', 'jesusssat@sdsu.edu', 'Executive']
-  ];
-
-//Storing Data
+];
+//Storing the data
 if (localStorage.getItem('employees') !== null) {
     employeeArray = JSON.parse(localStorage.getItem('employees'))
 }
-let addform=document.getElementById('addForm')
-let empTable=document.getElementById('empTable')
-let empCount=document.getElementById('empCount')
+let addform = document.getElementById('addForm')
+let empTable = document.getElementById('empTable')
+let empCount = document.getElementById('empCount')
 Gridbuilding()
-// Adding Data
+// Adding the data
 addform.addEventListener('submit', (e) => {
     e.preventDefault();
-    let empID       = parseInt(document.getElementById('id').value)
-    let empName     = document.getElementById('name').value
-    let empExt      = parseInt(document.getElementById('extension').value)
-    let empEmail    = document.getElementById('email').value
-    let empDept     = document.getElementById('department').value
+    let empID = parseInt(document.getElementById('id').value)
+    let empName = document.getElementById('name').value
+    let empExt = parseInt(document.getElementById('extension').value)
+    let empEmail = document.getElementById('email').value
+    let empDept = document.getElementById('department').value
     let arrNewEmployee = [empID, empName, empExt, empEmail, empDept]
     employeeArray.push(arrNewEmployee)
     Gridbuilding()
     addform.reset()
     addform.id.focus()
 })
-
-//Removing Data
+//Removing data
 empTable.addEventListener('click', (e) => {
     if (e.target.classList.contains('delete')) {
         if (confirm('Please confirm that you want to delete this employee from the system?')) {
@@ -41,15 +39,13 @@ empTable.addEventListener('click', (e) => {
         }
     }
 })
-
-// Building the Grid
-
+// Building the grid
 function Gridbuilding() {
     empTable.lastElementChild.remove()
     let tbody = document.createElement('tbody')
-    for (let employee of EmployeesArray) {
-        tbody.innerHTML += 
-        `<tr>
+    for (let employee of employeeArray) {
+        tbody.innerHTML +=
+            `<tr>
             <td>${employee[0]}</td>
             <td>${employee[1]}</td>
             <td>${employee[2]}</td>
@@ -58,10 +54,8 @@ function Gridbuilding() {
             <td><button class="btn btn-sm btn-danger delete">X</button></td>
         </tr>
         `}
-empTable.appendChild(tbody);
-empCount.value = `(${EmployeesArray.length})`
-
-//Storing Data
-    
-localStorage.setItem('employees', JSON.stringify(EmployeesArray))
+    empTable.appendChild(tbody);
+    empCount.value = `(${employeeArray.length})`
+    //Storing the data
+    localStorage.setItem('employees', JSON.stringify(employeeArray))
 }
